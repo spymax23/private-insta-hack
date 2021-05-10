d0d49b7089e8605768ea011fc522b36d4a1389e0
spymax23 <72801869+spymax23@users.noreply.github.com>
2021-05-10 20:14:32 +0500

    Create Privateinstahack.py

---
+++ Privateinstahack.py
@@ -0,0 +1,143 @@
+import sys
+
+import os
+
+import time
+
+import socket
+
+import random
+
+#Code Time
+
+from datetime import datetime
+
+now = datetime.now()
+
+hour = now.hour
+
+minute = now.minute
+
+day = now.day
+
+month = now.month
+
+year = now.year
+
+##############
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+bytes = random._urandom(1490)
+
+#############
+
+os.system("clear")
+
+os.system("figlet Instagram Hacking")
+
+print
+
+print "Author   : M Faizan (Cyber Expert) "
+
+print "Instagram Cookies Stealer : "
+
+print "github   : https://github.com/c-t-l"
+
+print
+
+ip = raw_input("Target ID : ")
+
+port = input("Hack Mode       : ")
+
+os.system("clear")
+
+os.system("figlet InstaHack")
+
+print "[                    ] Getting Into @pomiii80"
+
+time.sleep(5)
+
+print "[=====               ] Sniffing cookies"
+
+time.sleep(5)
+
+print "[==========          ] Hacked Successfully"
+
+time.sleep(5)
+
+print "[===============     ] Getting Followers list "
+
+time.sleep(5)
+
+print "[====================] Getting Blocked Users details"
+
+time.sleep(3)
+
+def main():
+
+	banner()
+
+print("     @shona.selly  ")
+
+print("       @_btwitssunaina_ ")
+
+print("      @capricornmindsx ")
+
+print("      @fahim_channar ")
+
+print("      @arfahamada41 ")
+
+print("     @ayman_bhattiiii  ")
+
+print("     @itx_hamxii804  ")
+
+print("      @sheeza3757 ")
+
+print("     @ayimas8  ")
+
+print("      @alimovie962 ")
+
+print("      @btw_im_falak ")
+
+print("     @wahabmalick94  ")
+
+print("       ")
+
+print("       ")
+
+print("       ")
+
+print("       ")
+
+print("       ")
+
+print("       ")
+
+print("       ")
+
+print("       ")
+
+print("       ")
+
+print("       ")
+
+print("       ")
+
+print("       ")
+
+print("       ")
+
+print("       ")
+
+print("       ")
+
+print("       ")
+
+print("       ")
+
+print("       ")
+
+print("       ")
+
+print("       ")
